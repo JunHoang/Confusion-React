fc934442d9eebb6f687c4249b997c53d65d8b650
Dun <kathymaidung@gmail.com>
2020-05-22 17:34:20 +0300

    ass1 task 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,22 +7,36 @@ class DishDetail extends Component {
         super(props);
     }
 
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <Card >
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        }
+        else {
+            return (
+                <div></div>
+            )
+        }
+
+    }
+
     render() {
         const dish = this.props.dish;
 
         return (
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
-                    <Card >
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    {this.renderDish(this.props.dish)}
                 </div>
                 <div className="col-12 col-md-5 m-1">
-
+                    
                 </div>
 
             </div>

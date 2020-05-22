bf5e2aa78abafcdd21ed498aef705fae49d0ed17
Dun <kathymaidung@gmail.com>
2020-05-22 18:48:05 +0300

    Assignment 1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,8 +27,44 @@ class DishDetail extends Component {
 
     }
 
+    renderComments(comments) {
+        if (comments==null) {
+            return (
+                <div></div>
+            )
+        }
+        const cmts = comments.map(comment => {
+            return(
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author},&nbsp; 
+                        {new Intl.DateTimeFormat('en-US', {
+                            year:'numeric',
+                            month: 'long',
+                            day: '2-digit'
+                        }).format(new Date(comment.date))}
+                    </p>
+                </li>
+            )
+        })
+
+        return(
+            <div>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">{cmts}</ul>
+            </div>
+        )
+
+    }
+
     render() {
         const dish = this.props.dish;
+
+        if (dish == null) {
+            return(
+                <div></div>
+            )
+        }
 
         return (
             <div className="row">
@@ -36,9 +72,8 @@ class DishDetail extends Component {
                     {this.renderDish(this.props.dish)}
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    
+                    {this.renderComments(this.props.dish.comments)}
                 </div>
-
             </div>
         )
     }

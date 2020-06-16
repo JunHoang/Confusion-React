4fb7ebca75cb969937ae32ae52b0eb30afef4a20
Dun <kathymaidung@gmail.com>
2020-06-16 13:49:04 +0300

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import {Card,CardImg,CardBody, CardTitle, CardText,Row, Col, Breadcrumb, BreadcrumbItem, Button, ModalHeader, ModalBody, Modal, Label } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardText, Row, Col, Breadcrumb, BreadcrumbItem, Button, ModalHeader, ModalBody, Modal, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors} from 'react-redux-form';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 function RenderDish({ dish }) {
     if (dish != null) {
@@ -48,37 +49,57 @@ function RenderComments({ comments, addComment, dishId }) {
         <div>
             <h4>Comments</h4>
             <ul className="list-unstyled">{cmts}</ul>
-            <CommentForm dishId={dishId} addComment={addComment}/>
+            <CommentForm dishId={dishId} addComment={addComment} />
         </div>
     )
 
 }
 
 const DishDetail = (props) => {
-    return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr />
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading/>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish} />
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={props.comments} 
-                        addComment={props.addComment}
-                        dishId={props.dish.id}/>
+        )
+    }
+    else if(props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
                 </div>
             </div>
-        </div>
-    );
+        )
+    }
+
+    else if (props.dish != null)
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id} />
+                    </div>
+                </div>
+            </div>
+        );
 }
 
 export default DishDetail;
@@ -96,7 +117,7 @@ export class CommentForm extends Component {
         }
 
         this.toggleModal = this.toggleModal.bind(this);
-        this.handleSubmit= this.handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     toggleModal() {
@@ -105,7 +126,7 @@ export class CommentForm extends Component {
         });
     }
 
-    handleSubmit(values){
+    handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
         this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
@@ -144,16 +165,16 @@ export class CommentForm extends Component {
                                             className="form-control"
                                             validators={{
                                                 required, minLength: minLength(3), maxLength: maxLength(15)
-                                            }}/>
+                                            }} />
                                         <Errors
                                             className="text-danger"
                                             model=".author"
                                             show="touched"
                                             messages={{
-                                                required:'Required ',
-                                                minLength:'Must be greater than 2 characters',
-                                                maxLength:'Must be 15 characters of less'
-                                            }} 
+                                                required: 'Required ',
+                                                minLength: 'Must be greater than 2 characters',
+                                                maxLength: 'Must be 15 characters of less'
+                                            }}
                                         />
                                     </Col>
                                 </Row>
@@ -163,12 +184,12 @@ export class CommentForm extends Component {
                                         <Control.textarea model=".comment" id="comment"
                                             name="comment" row="6"
                                             className="form-control"
-                                            validators={{required}}/>
-                                        <Errors 
+                                            validators={{ required }} />
+                                        <Errors
                                             className="text-danger"
                                             model=".comment"
                                             show="touched"
-                                            messages={{required:'Required'}}
+                                            messages={{ required: 'Required' }}
                                         />
                                     </Col>
                                 </Row>

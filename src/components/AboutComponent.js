4ec57b24453d59a23d226506420ffc7247c422c9
Dun <kathymaidung@gmail.com>
2020-07-04 17:42:03 +0300

    assignment 4 part 1

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 function RenderLeader({ leader }) {
     return (
         <div key={leader.id} className="col-12">
             <Media tag="li">
                 <Media left middle className="mt-1">
-                    <Media object src={leader.image} alt={leader.name} />
+                    <Media object src={baseUrl + leader.image} alt={leader.name} />
                 </Media>
                 <Media body className="ml-5">
                     <Media heading>{leader.name}</Media>
@@ -20,11 +22,28 @@ function RenderLeader({ leader }) {
 }
 
 function About(props) {
-
+    
     const leaders = props.leaders.map((leader) => {
-        return (
-            <RenderLeader leader={leader}/>
-        );
+        if (props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        } else if (props.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        } else
+            return (
+                <RenderLeader leader={leader} />
+            );
     });
 
     return (

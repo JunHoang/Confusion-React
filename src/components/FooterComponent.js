06c3b14566e5dbef229761d969af65734097de50
DungHoang <kathymaidung@gmail.com>
2021-01-13 12:29:23 +0200

    modify info

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -18,13 +18,12 @@ function Footer(props) {
                     <div className="col-7 col-sm-5">
                         <h5>Our Address</h5>
                         <address>
-                            121, Clear Water Bay Road<br />
-		              Clear Water Bay, Kowloon<br />
-		              HONG KONG<br />
-                            <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
-                            <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
-                            <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
-                                confusion@food.net</a>
+                        Raiviosuonmäki<br />
+		                Vantaa, FINLAND<br />
+                            <i className="fa fa-phone fa-lg"></i>: +358 465 712 157<br />
+                            <i className="fa fa-fax fa-lg"></i>: +358 465 712 157<br />
+                            <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:hungry_react@food.net">
+                                hungry_react@food.net</a>
                         </address>
                     </div>
                     <div className="col-12 col-sm-4 align-self-center">
@@ -40,7 +39,7 @@ function Footer(props) {
                 </div>
                 <div className="row justify-content-center">
                     <div className="col-auto">
-                        <p>© Copyright 2020 Ristorante Con Fusion</p>
+                        <p>© Copyright 2021 Hungry React</p>
                     </div>
                 </div>
             </div>

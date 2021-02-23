dca93a4858d1b636fe9dd3701b3f2f5a86ea5995
DungHoang <kathymaidung@gmail.com>
2021-02-23 18:50:14 +0200

    fixed Carousel

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,118 +1,198 @@
-import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { baseUrl } from '../shared/baseUrl';
-import { Loading } from './LoadingComponent';
-import { Stagger, Fade } from 'react-animation-components';
+import React, { useState } from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardBody,
+  CardHeader,
+  Media,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { baseUrl } from "../shared/baseUrl";
+import { Loading } from "./LoadingComponent";
+import { Stagger, Fade } from "react-animation-components";
+import "../App.css";
+
+const items = [
+  {
+    src: "https://source.unsplash.com/random/1024x600/?restaurant",
+  },
+  {
+    src: "https://source.unsplash.com/random/1025x601/?restaurant",
+  },
+  {
+    src: "https://source.unsplash.com/random/1026x602/?restaurant",
+  },
+];
 
 function RenderLeader({ leader }) {
-    return (
-        <div key={leader.id} className="col-12">
-            <Media tag="li">
-                <Media left middle className="mt-1">
-                    <Media object src={leader.image} alt={leader.name} />
-                </Media>
-                <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                </Media>
-            </Media>
-        </div>
-    );
+  return (
+    <div key={leader.id} className="col-12">
+      <Media tag="li">
+        <Media left middle className="mt-1">
+          <Media object src={leader.image} alt={leader.name} />
+        </Media>
+        <Media body className="ml-5">
+          <Media heading>{leader.name}</Media>
+          <p>{leader.designation}</p>
+          <p>{leader.description}</p>
+        </Media>
+      </Media>
+    </div>
+  );
 }
 
 function About(props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
 
-    const leaders = props.leaders.map((leader) => {
-        if (props.isLoading) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <Loading />
-                    </div>
-                </div>
-            );
-        } else if (props.errMess) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <h4>{props.errMess}</h4>
-                    </div>
-                </div>
-            );
-        } else
-            return (
-                <Stagger in>
-                    <Fade in>
-                        <RenderLeader leader={leader} />
-                    </Fade>
-                </Stagger>
-            );
-    });
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
 
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map((item) => {
     return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>About Us</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>About Us</h3>
-                    <hr />
-                </div>
-            </div>
-            <div className="row row-content">
-                <div className="col-12 col-md-6">
-                    <h2>Our History</h2>
-                    <p>Started in 2010, Hungry React Restaurant quickly established itself as a culinary icon par excellence in Finland. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Finland.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</p>
-                    <p>The restaurant traces its humble beginnings to <em>The Frying Pan</em>, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</p>
-                </div>
-                <div className="col-12 col-md-5">
-                    <Card>
-                        <CardHeader className="bg-primary text-white">Facts At a Glance</CardHeader>
-                        <CardBody>
-                            <dl className="row p-1">
-                                <dt className="col-6">Started</dt>
-                                <dd className="col-6">3 Feb. 2013</dd>
-                                <dt className="col-6">Major Stake Holder</dt>
-                                <dd className="col-6">FL Fine Foods Inc.</dd>
-                                <dt className="col-6">Last Year's Turnover</dt>
-                                <dd className="col-6">$1,250,375</dd>
-                                <dt className="col-6">Employees</dt>
-                                <dd className="col-6">40</dd>
-                            </dl>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col-12">
-                    <Card>
-                        <CardBody className="bg-faded">
-                            <blockquote className="blockquote">
-                                <p className="mb-0">You better cut the pizza in four pieces because
-                                    I'm not hungry enough to eat six.</p>
-                                <footer className="blockquote-footer">Yogi Berra,
-                                <cite title="Source Title">The Wit and Wisdom of Yogi Berra,
-                                    P. Pepe, Diversion Books, 2014</cite>
-                                </footer>
-                            </blockquote>
-                        </CardBody>
-                    </Card>
-                </div>
-            </div>
-            <div className="row row-content">
-                <div className="col-12">
-                    <h2>Corporate Leadership</h2>
-                </div>
-                <div className="col-12">
-                    <Media list>
-                        {leaders}
-                    </Media>
-                </div>
-            </div>
-        </div>
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} className="d-block w-100" />
+      </CarouselItem>
     );
+  });
+
+  const leaders = props.leaders.map((leader) => {
+    if (props.isLoading) {
+      return (
+        <div className="container">
+          <div className="row">
+            <Loading />
+          </div>
+        </div>
+      );
+    } else if (props.errMess) {
+      return (
+        <div className="container">
+          <div className="row">
+            <h4>{props.errMess}</h4>
+          </div>
+        </div>
+      );
+    } else
+      return (
+        <Stagger in>
+          <Fade in>
+            <RenderLeader leader={leader} />
+          </Fade>
+        </Stagger>
+      );
+  });
+
+  return (
+    <div className="container" id="about">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>About Us</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>About Us</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row row-content">
+        <div className="col-12 col-md-6">
+          <h2>Our History</h2>
+          <p>
+            Started in 2010, Hungry React Restaurant quickly established itself
+            as a culinary icon par excellence in Finland. With its unique brand
+            of world fusion cuisine that can be found nowhere else, it enjoys
+            patronage from the A-list clientele in Finland. Featuring four of
+            the best three-star Michelin chefs in the world, you never know what
+            will arrive on your plate the next time you visit us.
+          </p>
+          <p>
+            The restaurant traces its humble beginnings to{" "}
+            <em>The Frying Pan</em>, a successful chain started by our CEO, Mr.
+            Peter Pan, that featured for the first time the world's best
+            cuisines in a pan.
+          </p>
+          <Card className="mb-3">
+            <CardHeader className="bg-primary text-white">
+              Facts At a Glance
+            </CardHeader>
+            <CardBody>
+              <dl className="row p-1">
+                <dt className="col-6">Started</dt>
+                <dd className="col-6">3 Feb. 2013</dd>
+                <dt className="col-6">Major Stake Holder</dt>
+                <dd className="col-6">FL Fine Foods Inc.</dd>
+                <dt className="col-6">Last Year's Turnover</dt>
+                <dd className="col-6">$1,250,375</dd>
+                <dt className="col-6">Employees</dt>
+                <dd className="col-6">40</dd>
+              </dl>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col-12 col-md-6">
+          <img
+            src="https://images.unsplash.com/photo-1584299574144-b5f8ac209707?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fHJlc3RhdXJhbnR8ZW58MHwyfDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"
+            alt=""
+            className="img-fluid rounded-circle d-none d-md-block about-img"
+          />
+        </div>
+      </div>
+      <div className="col-12">
+        <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+          <CarouselIndicators
+            items={items}
+            activeIndex={activeIndex}
+            onClickHandler={goToIndex}
+          />
+          {slides}
+          <CarouselControl
+            direction="prev"
+            directionText="Previous"
+            onClickHandler={previous}
+          />
+          <CarouselControl
+            direction="next"
+            directionText="Next"
+            onClickHandler={next}
+          />
+        </Carousel>
+      </div>
+      <div className="row row-content">
+        <div className="col-12">
+          <h2>Corporate Leadership</h2>
+        </div>
+        <div className="col-12">
+          <Media list>{leaders}</Media>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About;    
+export default About;
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -74,22 +74,38 @@ class Header extends Component {
             <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar>
                 <NavItem>
-                  <NavLink className="nav-link" to="/home">
+                  <NavLink
+                    activeClassName="text-warning"
+                    className="nav-link"
+                    to="/home"
+                  >
                     <span className="fa fa-home fa-lg"> Home</span>
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/aboutus">
+                  <NavLink
+                    className="nav-link"
+                    activeClassName="text-warning"
+                    to="/aboutus"
+                  >
                     <span className="fa fa-info fa-lg"> About Us</span>
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/menu">
+                  <NavLink
+                    className="nav-link"
+                    activeClassName="text-warning"
+                    to="/menu"
+                  >
                     <span className="fa fa-list fa-lg"> Menu</span>
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink className="nav-link" to="/contactus">
+                  <NavLink
+                    className="nav-link"
+                    activeClassName="text-warning"
+                    to="/contactus"
+                  >
                     <span className="fa fa-address-card fa-lg">
                       {" "}
                       Contact Us

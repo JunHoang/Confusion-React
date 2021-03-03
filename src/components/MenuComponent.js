d36065fdd5899ae4e66f22584a22a988820aa317
DungHoang <kathymaidung@gmail.com>
2021-03-03 14:34:01 +0200

    Change format menu

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,68 +1,84 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { Loading } from './LoadingComponent';
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  Media,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Loading } from "./LoadingComponent";
 
 function RenderMenuItem({ dish }) {
-    return (
-        <Card>
-            <Link to={`/menu/${dish._id}`}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
-            </Link>
+  return (
+    <div id="menu" key={dish._id} className="col-12">
+      <Media tag="li" className="py-2">
+        <Card
+          className="mr-4 border-0"
+          style={{ width: "150px", height: "150px" }}
+        >
+          <Link to={`/menu/${dish._id}`}>
+            <CardImg src={dish.image} alt={dish.name} />
+          </Link>
         </Card>
-    );
+        <Media body className="mr-5">
+          <Media heading>{dish.name}</Media>
+          <p className="text-muted">{dish.description}</p>
+        </Media>
+        <h5>{dish.price}</h5>
+      </Media>
+      <hr />
+    </div>
+  );
 }
 
 const Menu = (props) => {
-    const menu = props.dishes.dishes.map((dish) => {
-        return (
-            <div key={dish._id} className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
-            </div>
-        );
-    });
+  const menu = props.dishes.dishes.map((dish) => {
+    return (
+      <div key={dish._id} className="col-12 col-md-6">
+        <RenderMenuItem dish={dish} onClick={props.onClick} />
+      </div>
+    );
+  });
 
-    if (props.dishes.isLoading) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <Loading />
-                </div>
-            </div>
-        )
-    }
-    else if (props.dishes.errMess) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h4>{props.dishes.errMess}</h4>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-    else
-        return (
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Menu</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>Menu</h3>
-                        <hr />
-                    </div>
-                </div>
-                <div className="row">
-                    {menu}
-                </div>
-            </div>
-        );
-}
+  if (props.dishes.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.dishes.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h4>{props.dishes.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  } else
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Menu</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>Menu</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">{menu}</div>
+      </div>
+    );
+};
 
 export default Menu;
